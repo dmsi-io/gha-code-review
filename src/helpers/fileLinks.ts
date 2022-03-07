cfb82021b9f3dbfd7e305fd61e9fd7885f3e9134
Elijah Biede <6586509+hbiede@users.noreply.github.com>
2022-03-07 16:26:33 -0600

    Fix issues with changelogs used in nested directories or when not adding to a log

--- src/helpers/fileLinks.ts
+++ src/helpers/fileLinks.ts
@@ -1,15 +1,21 @@
 export const commentPath = (
-  c: { line?: number; path: string },
+  c:
+    | { directory: boolean; line?: never; path: string }
+    | { directory?: never; line: number; path: string },
   repoOwner: string,
   repo: string,
   branch: string,
   prefix = '- ',
 ) => {
   if (branch && repo && repoOwner) {
-    return `${prefix}[\`${c.path}${
+    return `${prefix}[\`${c.directory && (c.path === '' || c.path === '.') ? './' : c.path}${
       c.line && c.line > 0 ? `:${c.line}` : ''
-    }\`](https://github.com/${repoOwner}/${repo}/blob/${branch}/${c.path.replace(/^\.\//, '')}${
-      c.line && c.line > 0 ? `#L${c.line}` : ''
+    }\`](https://github.com/${repoOwner}/${repo}/${
+      c.directory
+        ? `tree/${branch}`
+        : `blob/${branch}/${c.path.replace(/^\.\//, '')}${
+            c.line && c.line > 0 ? `#L${c.line}` : ''
+          }`
     })`;
   }
 
